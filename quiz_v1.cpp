074d6be479c2a1f0adcbece8180e82ccc2f0e387
pratikghosh2000 <1804034@kiit.ac.in>
2020-07-04 01:57:30 +0530

    trying to read a file

--- quiz_v1.cpp
+++ quiz_v1.cpp
@@ -1,8 +1,26 @@
 #include<stdio.h>
+#include<iostream>
+#include<fstream>
+#include<string>
 using namespace std;
 
 
 int main()
 {
-	printf("hello");
+ifstream infile;
+infile.open("C:\Users\KIIT\Desktop\quizsystem\\trial.txt");
+if(infile.fail())
+{
+	cerr<<"error"<<endl;
+	exit(1);
+	
+}
+string item;
+int count=0;
+while(!infile.eof())
+{
+	infile>>item;
+	count++;
+}
+cout<<count;
 }
