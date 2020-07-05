f4a356d05399411aee0e089de3db8740cdf1bc1f
pratikghosh2000 <1804034@kiit.ac.in>
2020-07-05 13:02:54 +0530

    question adding using python

---
+++ qsn2.py
@@ -0,0 +1,36 @@
+class question:
+    def __init__(self,x):
+        self.x=x
+    def getqs(self):
+        file1 = open("qsn.txt",'a')
+        c = input("enter the questions to be added\n");
+        file1.write("\n")
+        file1.write(c)
+        file1.close()
+    def dispqs(self):
+        print("contents of question file");
+        file1 = open("qsn.txt", 'r')
+        line1 = file1.readline()
+        while (line1 != ""):
+            print(line1)
+            line1 = file1.readline()
+        file1.close()
+
+qsn=question(1)
+number1=int(input("enter the number of questions to be added"));
+count=1
+while(count<=number1):
+    con=input("press any key to continue or press -1 to stop");
+    if con=="-1":
+        print("contents of question file");
+        file1 = open("qsn.txt", 'r')
+        line1 = file1.readline()
+        while (line1 != ""):
+            print(line1)
+            line1 = file1.readline()
+        file1.close()
+        exit()
+    else:
+        qsn.getqs()
+        count=count+1
+qsn.dispqs()
