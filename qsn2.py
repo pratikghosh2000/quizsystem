0d842c725197d9d21a55ac3d67edfc54d817c4b9
pratikghosh2000 <1804034@kiit.ac.in>
2020-07-08 12:13:53 +0530

    password protection

--- qsn2.py
+++ qsn2.py
@@ -1,3 +1,6 @@
+import hashlib
+import sys
+password="095f26e6c7c467c5ecce3205a0dc90f6dc4868e8ddfe7a21829e91d47e386676"  #pratik
 class question:
     def __init__(self,x):
         self.x=x
@@ -15,22 +18,31 @@ class question:
             print(line1)
             line1 = file1.readline()
         file1.close()
-
-qsn=question(1)
-number1=int(input("enter the number of questions to be added"));
-count=1
-while(count<=number1):
-    con=input("press any key to continue or press -1 to stop");
-    if con=="-1":
-        print("contents of question file");
-        file1 = open("qsn.txt", 'r')
-        line1 = file1.readline()
-        while (line1 != ""):
-            print(line1)
+def main():
+    qsn=question(1)
+    number1=int(input("enter the number of questions to be added"));
+    count=1
+    while(count<=number1):
+        con=input("press any key to continue or press -1 to stop");
+        if con=="-1":
+            print("contents of question file");
+            file1 = open("qsn.txt", 'r')
             line1 = file1.readline()
-        file1.close()
-        exit()
+            while (line1 != ""):
+                print(line1)
+                line1 = file1.readline()
+            file1.close()
+            exit()
+        else:
+            qsn.getqs()
+            count=count+1
+    qsn.dispqs()
+
+while True:
+    name=input("Enter password: ")
+    #name.encode('utf-8')
+    if hashlib.sha256(name.encode('utf-8')).hexdigest()==password:
+        print("You are authorized and allowed to add questions")
+        main()
     else:
-        qsn.getqs()
-        count=count+1
-qsn.dispqs()
+        print("Error in password. You are not authorized")
